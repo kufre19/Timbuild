4a815f820c11ed0813e2c78a014bb80a33a62637
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-07-13 02:50:59 +0100

    formated main file

--- app/Http/Controllers/BotAbilities/Main.php
+++ app/Http/Controllers/BotAbilities/Main.php
@@ -8,7 +8,7 @@ use App\Models\ScheduleMenu;
 use Illuminate\Http\Request;
 
 
- 
+
 /**
  * this main file will be used to map main menu selection to journies to be started 
  */
@@ -23,23 +23,23 @@ class Main extends BotFunctionsGeneralFunctions implements AbilityInterface
         "Visit the TimBuild SA’s website",
         "Read our Competition Rules for Entry"
     ];
-   
+
 
 
 
     public function begin_func()
     {
-       $menu_main = $this->MenuArrayToObj($this->main_menu);
-       $text_menu = new TextMenuSelection($menu_main);
-       $menu_message = <<<MSG
+        $menu_main = $this->MenuArrayToObj($this->main_menu);
+        $text_menu = new TextMenuSelection($menu_main);
+        $menu_message = <<<MSG
        Hello There!
        Welcome to Timbuild South Africa.
        Please choose from one of the following options:
        MSG;
-       $this->set_session_route("Main");
-       $text_menu->send_menu_to_user($menu_message);
-       $this->go_to_next_step();
-       $this->ResponsedWith200();
+        $this->set_session_route("Main");
+        $text_menu->send_menu_to_user($menu_message);
+        $this->go_to_next_step();
+        $this->ResponsedWith200();
     }
 
 
@@ -48,9 +48,9 @@ class Main extends BotFunctionsGeneralFunctions implements AbilityInterface
         // this should the hello message with main menu
     }
 
-   
 
-   
+
+
     public function DoSelection()
     {
         // check first if the selection is in the list
@@ -58,27 +58,23 @@ class Main extends BotFunctionsGeneralFunctions implements AbilityInterface
         $text_menu_selection = new TextMenuSelection($menu_main);
         $check = $text_menu_selection->check_expected_response($this->user_message_original);
 
-        if($check)
-        {
+        if ($check) {
             // do the menu selection
-           $user_selected = $this->user_message_original;
-           $user_selected_lowered = $this->user_message_lowered;
+            $user_selected = $this->user_message_original;
+            $user_selected_lowered = $this->user_message_lowered;
 
-           if($user_selected == "1" || $user_selected_lowered == "Enter our latest Competition" )
-           {
-                $this->send_post_curl($this->make_text_message("coming soon!",$this->userphone));
+            if ($user_selected == "1" || $user_selected_lowered == "Enter our latest Competition") {
+                $this->send_post_curl($this->make_text_message("coming soon!", $this->userphone));
                 $this->ResponsedWith200();
-           }
+            }
 
-           if($user_selected == "2" || $user_selected_lowered == "Locate your nearest TimBuild Store" )
-           {
-                $this->send_post_curl($this->make_text_message("coming soon!",$this->userphone));
+            if ($user_selected == "2" || $user_selected_lowered == "Locate your nearest TimBuild Store") {
+                $this->send_post_curl($this->make_text_message("coming soon!", $this->userphone));
                 $this->ResponsedWith200();
-           }
+            }
 
-           if($user_selected == "3" || $user_selected_lowered == "Visit the TimBuild SA’s website" )
-           {
-            $message = <<<MSG
+            if ($user_selected == "3" || $user_selected_lowered == "Visit the TimBuild SA’s website") {
+                $message = <<<MSG
             You can visit our main TimBuild SA website by clicking the link below:
 
             www.timbuild.co.za 
@@ -86,15 +82,14 @@ class Main extends BotFunctionsGeneralFunctions implements AbilityInterface
             NOTE: Reply MENU at any time to return to our main menu.
 
             MSG;
-            $txt_msg = $this->make_text_message($message,$this->userphone,true);
-            $this->send_post_curl($txt_msg);
-            $this->ResponsedWith200();
-           }
+                $txt_msg = $this->make_text_message($message, $this->userphone, true);
+                $this->send_post_curl($txt_msg);
+                $this->ResponsedWith200();
+            }
 
-           if($user_selected == "4" || $user_selected_lowered == "Read our Competition Rules for Entry" )
-           {
-           
-               $message = <<<MSG
+            if ($user_selected == "4" || $user_selected_lowered == "Read our Competition Rules for Entry") {
+
+                $message = <<<MSG
                Our latest rules of entry into our competitions can be found on our main TimBuild SA website by clicking the link below:
 
                www.timbuild.co.za/competitions 
@@ -102,15 +97,11 @@ class Main extends BotFunctionsGeneralFunctions implements AbilityInterface
                NOTE: Reply MENU at any time to return to our main menu.
 
                MSG;
-               $txt_msg = $this->make_text_message($message,$this->userphone,true);
-               $this->send_post_curl($txt_msg);
-               $this->ResponsedWith200();
-           }
+                $txt_msg = $this->make_text_message($message, $this->userphone, true);
+                $this->send_post_curl($txt_msg);
+                $this->ResponsedWith200();
+            }
         }
-        
-
-
-
     }
 
 
@@ -124,7 +115,4 @@ class Main extends BotFunctionsGeneralFunctions implements AbilityInterface
     {
         return "ok";
     }
-
-
-
 }
