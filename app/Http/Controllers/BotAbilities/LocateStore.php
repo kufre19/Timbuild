757ce4387d09b9c38408131e20db41dbb613bc8f
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-07-13 04:43:41 +0100

    test gettig of storelocation listed

--- app/Http/Controllers/BotAbilities/LocateStore.php
+++ app/Http/Controllers/BotAbilities/LocateStore.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\BotAbilities;
 use App\Http\Controllers\BotFunctions\GeneralFunctions;
 use App\Traits\GeneralAbilities;
 
-class LocateStore extends GeneralFunctions implements AbilityInterface{
+class LocateStore extends GeneralFunctions implements AbilityInterface
+{
     use GeneralAbilities;
 
-    public $steps = ["begin_func", "CheckRegion", ""];
+    public $steps = ["begin_func", "getStoreLocation", ""];
+    public const USER_REGION = "user_region";
 
 
     public function begin_func()
@@ -19,17 +21,47 @@ class LocateStore extends GeneralFunctions implements AbilityInterface{
         Please select your region to make your store selection easier. Your need only reply with the corresponding number:
         MSG;
         $this->listRegion($message);
+
+        $this->returnHomeMessage();
+
         $this->go_to_next_step();
         $this->ResponsedWith200();
-
-        
     }
 
 
-    public function CheckRegion()
+    public function getStoreLocation()
     {
-        $this->send_post_curl($this->make_text_message($this->user_message_original,$this->userphone));
-        $this->ResponsedWith200();
+        // $this->send_post_curl($this->make_text_message($this->user_message_original,$this->userphone));
+        // $this->ResponsedWith200();
+
+        // check first for selection to be correct
+        $user_selected = $this->user_message_original;
+        $this->CheckRegionSelected($user_selected);
+
+      
+        // second check for selection of others
+        if ($user_selected == "6" || $this->user_message_lowered == "others") {
+            $message = <<<MSG
+            We’re sorry you’re located in a province we currently do not have any stores, but we’re positive one will be opening soon.
+            MSG;
+            $this->send_post_curl($this->make_text_message($message, $this->userphone));
+            $this->returnHomeMessage();
+            $this->ResponsedWith200();
+        }
+
+          // store collected region
+          $this->storeAnswerToSession(self::USER_REGION);
+
+          $store_menu = $this->listStoreInRegion($user_selected);
+          $message = <<<MSG
+          Fantastic. Here is a list of our stores located in your chosen province.
+          MSG;
+          $this->sendstoreMenu($store_menu,$message);
+          $this->returnHomeMessage();
+          $this->ResponsedWith200();
+
+
+
     }
 
     function call_method($key)
--- app/Traits/GeneralAbilities.php
+++ app/Traits/GeneralAbilities.php
@@ -3,6 +3,7 @@ namespace App\Traits;
 
 use App\Http\Controllers\BotFunctions\TextMenuSelection;
 use App\Models\Region;
+use App\Models\StoreInfo;
 
 trait GeneralAbilities
 {
@@ -22,6 +23,64 @@ trait GeneralAbilities
         $txt_menu = new TextMenuSelection($region_obj);
        
         $txt_menu->send_menu_to_user($message);
+
+    }
+
+
+    public function CheckRegionSelected($response)
+    {
+        $regions_Arr = [];
+        $region_model = new Region();
+        $regions = $region_model->select("region")->get();
+        foreach ($regions as $region => $value) {
+            array_push($regions_Arr,$value['region']);
+        }
+        $region_obj = $this->MenuArrayToObj($regions_Arr);
+        $txt_menu = new TextMenuSelection($region_obj);
+       
+        $check = $txt_menu->check_expected_response($response);
+        if($check)
+        {
+            return true;
+        }
+
+    }
+
+    public function listStoreInRegion($region)
+    {
+        if(!is_numeric($region))
+        {
+            $region_model = new Region();
+            $region_select = $region_model->select("id")->where('region',$region)->first();
+            $region = $region_select->id;
+
+        }
+        $store_model = new StoreInfo();
+        $stores = $store_model->select("location")->where("region_id",$region)->get();
+        $stores_Arr = [];
+
+        foreach ($stores as $store => $value) {
+            array_push($stores_Arr,$value['location']);
+        }
+        // extra data that's not saved in db 
+        array_push($stores_Arr,"Go Back to Province selection");
+
+        $store_obj = $this->MenuArrayToObj($stores_Arr);
+        return $store_obj;
+
+
+    }
+
+    public function sendstoreMenu($store_obj,$message)
+    {
+        $txt_menu = new TextMenuSelection($store_obj);
+        $txt_menu->send_menu_to_user($message);
+       
+    }
+
+    public function returnHomeMessage ()
+    {
+        $this->send_post_curl($this->make_text_message("NOTE: Reply MENU at any time to return to our main menu.",$this->userphone));
 
     }
 }
