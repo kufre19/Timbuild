8513e5a33d09bd294de3c1da6ba3fd3910a340d5
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-07-19 21:47:04 +0100

    added password

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Entries;
 use App\Models\Region;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,19 +16,49 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get("/",function(){
+    return view("login");
+});
+
+Route::post("login",function(Request $request){
+    $pass = $request->input("pass");
+    if($pass == "9MXtpUBANu")
+    {
+        session()->put("loggin",true);
+        return redirect()->to("/entries");
+    }else{
+        return redirect()->to("/");
+
+    }
+});
+
+Route::get('/entries', function () {
+    if(!session()->get("loggin") || !session()->has("loggin"))
+    {
+        return redirect()->to("/");
+
+    }
     $entries_model = new Entries();
     $entries = $entries_model->paginate(20);
     return view('index',compact("entries"));
 });
 
 
-Route::get("test", function(){
-    $regions_Arr = [];
-        $region_model = new Region();
-        $regions = $region_model->select("region")->get();
-        foreach ($regions as $region => $value) {
-            array_push($regions_Arr,$value['region']);
-        }
-    dd($regions_Arr);
-});
+// Route::get("test", function(){
+//     $headers = [
+//         "Content-type"        => "text/csv",
+//         "Content-Disposition" => "attachment; filename=output_chat.csv", // <- name of file
+//         "Pragma"              => "no-cache",
+//         "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
+//         "Expires"             => "0",
+//     ];
+//     $columns  = ['from_user', 'to_user', 'message', 'date_added'];
+//     $callback = function () use ($result, $columns) {
+//         $file = fopen('php://output', 'w'); //<-here. name of file is written in headers
+//         fputcsv($file, $columns);
+//         foreach ($result as $res) {
+//             fputcsv($file, [$res->from_user, $res->to_user, $res->message, $res->date_added]);
+//         }
+//         fclose($file);
+//     };
+// });
