6cbf090d2d81ed3efa957aca233efc65a12d20ab
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-07-20 11:15:05 +0100

    test mailing

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Models\Entries;
 use App\Models\Region;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,35 +18,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get("/",function(){
+Route::get("/", function () {
     return view("login");
 });
 
-Route::post("login",function(Request $request){
+Route::post("login", function (Request $request) {
     $pass = $request->input("pass");
-    if($pass == "9MXtpUBANu")
-    {
-        session()->put("loggin",true);
+    if ($pass == "9MXtpUBANu") {
+        session()->put("loggin", true);
         return redirect()->to("/entries");
-    }else{
+    } else {
         return redirect()->to("/");
-
     }
 });
 
 Route::get('/entries', function () {
-    if(!session()->get("loggin") || !session()->has("loggin"))
-    {
+    if (!session()->get("loggin") || !session()->has("loggin")) {
         return redirect()->to("/");
-
     }
     $entries_model = new Entries();
     $entries = $entries_model->paginate(15);
-    return view('index',compact("entries"));
+    return view('index', compact("entries"));
 });
 
 
-Route::get("download", function(){
+Route::get("download", function () {
     $entries_model = new Entries();
     $result = $entries_model->get();
     $headers = [
@@ -54,14 +52,35 @@ Route::get("download", function(){
         "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
         "Expires"             => "0",
     ];
-    $columns  = ['First Name', 'Last Name',"Email","Phone","Region"," Closer Store","Project","Industry","Connect to Store","DIY'ER","Contractor",];
+    $columns  = ['First Name', 'Last Name', "Email", "Phone", "Region", " Closer Store", "Project", "Industry", "Connect to Store", "DIY'ER", "Contractor",];
     $callback = function () use ($result, $columns) {
         $file = fopen('php://output', 'w'); //<-here. name of file is written in headers
         fputcsv($file, $columns);
         foreach ($result as $res) {
-            fputcsv($file, [$res->first_name, $res->last_name,$res->email,$res->phone,$res->region,$res->store_closes,$res->project,$res->industry,$res->connect_to_store,$res->is_diy_customer,$res->is_contractor,]);
+            fputcsv($file, [$res->first_name, $res->last_name, $res->email, $res->phone, $res->region, $res->store_closes, $res->project, $res->industry, $res->connect_to_store, $res->is_diy_customer, $res->is_contractor,]);
         }
         fclose($file);
     };
     return response()->stream($callback, 200, $headers);
+});
+
+
+Route::get("test", function () {
+    $data["email"] = "whitemaxwell5@gmail.com";
+    $data["title"] = "techsolutionstuff.com";
+
+    $files = [
+        public_path('entries.csv'),
+    ];
+
+    Mail::send('mail.entries', $data, function ($message) use ($data, $files) {
+        $message->to($data["email"])
+            ->subject($data["title"]);
+
+        foreach ($files as $file) {
+            $message->attach($file);
+        }
+    });
+
+    echo "Mail send successfully !!";
 });
