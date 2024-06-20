668bdc6f2a59094b40300a9d3785564bcdd16c50
Emmanuel kufre <whitemaxwell5@gmail.com>
2024-06-20 16:17:10 +0100

    sal

--- app/Http/Controllers/BotAbilities/OnlyWesternCapeLocation.php
+++ app/Http/Controllers/BotAbilities/OnlyWesternCapeLocation.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\BotAbilities;
 
 use App\Http\Controllers\BotFunctions\GeneralFunctions;
+use App\Http\Controllers\BotFunctions\TextMenuSelection;
 use App\Traits\GeneralAbilities;
 
 class OnlyWesternCapeLocation extends GeneralFunctions implements AbilityInterface
@@ -12,18 +13,26 @@ class OnlyWesternCapeLocation extends GeneralFunctions implements AbilityInterfa
     public $steps = ["begin_func", "getStoreLocationInfo", "getConsent","CheckConsentSelection"];
     public const USER_REGION = "user_region";
     public const STORE_SELECTED = "store_selected";
-    // public $main_menu = [
-    //     "TimBuild Woodstock",
-    //     "TimBuild Somerset West",
-    //     "TimBuild Stellenbosch",
-    // ];
+    public $main_menu = [
+        "TimBuild Woodstock",
+        "TimBuild Somerset West",
+        "TimBuild Stellenbosch",
+    ];
 
 
 
     public function begin_func()
     {
+        $menu_main = $this->MenuArrayToObj($this->main_menu);
+        $text_menu = new TextMenuSelection($menu_main);
+        $menu_message = <<<MSG
+        Hello There!
+        Welcome to TimBuild Western Cape.
+        Please choose from one of the following options to locate your nearest store:
+        MSG;
+        // $this->set_session_route("Main");
         $this->set_session_route("OnlyWesternCapeLocation");
-       
+        $text_menu->send_menu_to_user($menu_message);
         $this->go_to_next_step();
         $this->ResponsedWith200();
     }
